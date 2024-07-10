18039d294e41a129803560fdd9514862cca52d43
samson ogbonna <samsonogbonna95@gmail.com>
2024-07-10 12:34:50 +0100

    committing more channges

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import questions from "./question.json";
-import Refresh from "./components/Refresh"; import Result from "./components/Result";
-;
-
+import Refresh from "./components/Refresh";
+import Result from "./components/Result";
 
 
 function App() {
@@ -18,14 +17,76 @@ function App() {
   const [wrongAns, setWrongAns] = useState(0);
   const [percentage, setPercentage] = useState("");
   const [total, setTotal] = useState(10)
+  const [score, setScore] = useState(parseInt(localStorage.getItem("attempt")));
+  const [greeting, setGreeting] = useState("")
+
+
+  const handleLoad = (e) => {
+    console.log("on load", e);
+
+    //get the user's name from local storage, if it exists
+    let userName = localStorage.getItem("name");
+
+
+    //check if the number of attempt has reached 3 
+
+    const numberOfAttempts = 3
+    const getNumber = parseInt(localStorage.getItem("attempt"));
+
+    while (getNumber >= numberOfAttempts) {
+      alert("You have reached the attempt limit for today,try again tommorrow.");
+      return;
+    }
+
+
+
+
+
+    if (userName) {
+      setGreeting(
+        <h1 className="text-white text-2xl font-bold text-center">
+          {`welcome back ${userName}`} </h1>
+      )
+
+    } else {
+
+      userName = prompt("Please enter your name");
+      localStorage.setItem("name", userName);
+      localStorage.setItem("attempt", 0);
+      setGreeting(
+        <h1 className="text-white text-2xl font-bold text-center">
+          {`welcome ${userName}`} </h1>
+      )
+    }
+
+    if (userName === "") {
+      alert("Please enter your name")
+      userName = prompt("Please enter your name");
+      localStorage.setItem("name", userName);
+      setGreeting(
+        <h1 className="text-white text-2xl font-bold text-center">
+          {`welcome ${userName}`} </h1>
+      )
+    } else if (userName === null) {
+      alert("Please enter your name")
+      userName = prompt("Please enter your name");
+      localStorage.setItem("name", userName);
+      setGreeting(
+        <h1 className="text-white text-2xl font-bold text-center">
+          {`welcome ${userName}`} </h1>
+      )
+    } else {
+      userName = ""
+    }
+  };
+
 
   useEffect(() => {
-    const handleLoad = () => {
-      setLoadQuestions(questions);
-    };
-    window.addEventListener('load', handleLoad);
-    return () => window.removeEventListener('load', handleLoad);
+    setLoadQuestions(questions);
+    window.addEventListener("load", handleLoad)
   }, []);
+
+
 
   const getSelectedButton = (e, quest, userAnswer) => {
     const correctAnswer = quest.answer;
@@ -42,11 +103,26 @@ function App() {
 
     e.target.parentNode.setAttribute("disabled", "");
 
+
+    const numberOfQuestions = questions.length;
+    if (answeredCount === 9 && numberOfQuestions) {
+      setScore(prevScore => prevScore + 1);
+
+      //get current attempt count from local storage
+      const currentAttempt = localStorage.getItem("attempt");
+      localStorage.setItem("attempt", parseInt(currentAttempt) + 1);
+    }
+
+
+
+
+
     setAnsweredCount(prevCount => prevCount + 1);
     setAnsweredQuestions(prevState => ({
       ...prevState,
       [quest.question]: true
     }));
+
   };
 
   const generateRandomString = () => Math.random().toString(36).substr(2, 5);
@@ -76,8 +152,10 @@ function App() {
 
   return (
     <>
+
       <div className="w-80 mx-10 md:w-1/2  md:mx-auto mt-5" id="top-page">
-        <h1 className="text-3xl text-center text-green-600 font-bold">Simple Question And Answer</h1>
+        <h1 className="text-3xl text-center text-green-600 font-bold">Simple Question
+          And Answer</h1>
         <p className="text-xl text-center text-green-500 mt-2">
           Click the detail arrow to view the answer.
         </p>
@@ -86,11 +164,16 @@ function App() {
         </p>
       </div>
 
+      <div className="bg-indigo-300 block mx-auto mt-3
+       text-center text-white font-bold w-10 rounded-md">{score}</div>
+
+      <div className="bg-orange-400 block mx-auto w-64 mt-3 rounded-md">{greeting}</div>
+
       <div className="ms-5 w-80 md:w-1/2 md:mx-auto mt-5">
         <p className="text-orange-400 text-xl font-semibold text-center">
           You have answered <span className="text-green-700">
             {answeredCount}</span> out of
-          <span className="text-green-700">{questions.length}</span> questions.
+          <span className="text-green-700"> {questions.length}</span> questions.
         </p>
         {answeredCount == questions.length && <a>
           <button onClick={computeResult} className="bg-green-700 text-white text-md 
@@ -142,4 +225,6 @@ function App() {
   );
 }
 
+
 export default App;
+
